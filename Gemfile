d7d09e1cfe298045edc49a5881d13316798b52df
naoymatsuda <matsudanaoyuki@MATSUDA-no-MacBook-Pro.local>
2016-06-12 23:42:06 +0900

    commit

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootstrap-sass'
 gem 'haml-rails'
 gem 'erb2haml'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
- devise_for :users
-
- root 'prototypes#index'
- get 'prototype/index' => 'prototypes#index'
- get 'prototype/show' => 'prototypes#show'
- get 'prototype/new' => 'prototypes#new'
-end
+  devise_for :users
+  root 'prototypes#index'
+  get 'prototype/index' => 'prototypes#index'
+  get 'prototype/show' => 'prototypes#show'
+  get 'prototype/new' => 'prototypes#new'
