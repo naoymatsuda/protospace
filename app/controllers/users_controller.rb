475c40542b8577c4c639d6de33f6a0f469da5140
naoymatsuda <matsudanaoyuki@MATSUDA-no-MacBook-Pro.local>
2016-06-30 18:15:14 +0900

    fix_editfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,13 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    current_user.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :avatar, :email, :member, :profile, :works)
+  end
+
 end
