53fa724dc545ccf3f806e39acc176932f8c79a71
naoymatsuda <matsudanaoyuki@MATSUDA-no-MacBook-Pro.local>
2016-07-04 08:59:15 +0900

    fix_user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
 
+before_action :authenticate_user!
+
   def edit
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def update
-    current_user.update(update_params)
+    @user = User.find(params[:id])
+    if @user.update(update_params)
+      redirect_to action: :show
+    else
+     render action: :edit
+    end
   end
 
   private
+
   def update_params
     params.require(:user).permit(:name, :avatar, :email, :member, :profile, :works)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show, :edit]
-  
+  resources :users, only: [:show, :edit, :update]
+
   root 'prototypes#index'
   get 'prototype/index' => 'prototypes#index'
   get 'prototype/show' => 'prototypes#show'
