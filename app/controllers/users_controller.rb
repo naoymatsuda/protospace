14eaf533223299b4ef7e2af6e2345ea2308fbeae
naoymatsuda <matsudanaoyuki@MATSUDA-no-MacBook-Pro.local>
2016-07-05 10:56:51 +0900

    fix_usersfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
 
 before_action :authenticate_user!
+before_action :set_user
 
   def edit
     redirect_to new_user_session_path unless user_signed_in?
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(update_params)
-      redirect_to action: :show
+      redirect_to :only => [action: :show]
     else
-     render action: :edit
+      render :only => [action: :edit]
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def update_params
     params.require(:user).permit(:name, :avatar, :email, :member, :profile, :works)
