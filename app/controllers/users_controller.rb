99d139dc7066174b88363963247aa2d63abfa664
naoymatsuda <matsudanaoyuki@MATSUDA-no-MacBook-Pro.local>
2016-07-06 22:27:38 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-before_action :authenticate_user!, only: [:edit :update]
+before_action :authenticate_user!, only: [:edit, :update]
 before_action :set_user,only: [:update]
 
   def edit
