88a097255120bccfa123f481f82eeabe352b7888
naoymatsuda <naoy.matsuda@gmail.com>
2016-07-24 11:33:58 +0900

    edit flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(update_params)
-      redirect_to ({ action: :show }), notice: 'Your prototype was successfully updated'
+      redirect_to ({ action: :show }), notice: 'your user information was updated'
     else
       render action: :edit
     end
