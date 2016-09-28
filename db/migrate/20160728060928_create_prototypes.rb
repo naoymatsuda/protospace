499a70b09c0da1130c433ee960af3a1a5b303ee5
naoymatsuda <naoy.matsuda@gmail.com>
2016-09-28 16:04:07 +0900

    commit prototypemodel

--- db/migrate/20160728060928_create_prototypes.rb
+++ db/migrate/20160728060928_create_prototypes.rb
@@ -1,11 +1,11 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-      t.string title
-      t.string catch_copy
-      t.string concept
-      t.string user_id
-      t.created_at timestamps
+      t.string :title
+      t.string :catch_copy
+      t.string :concept
+      t.string :user_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605151908) do
+ActiveRecord::Schema.define(version: 20160728060928) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "catch_copy", limit: 255
+    t.string   "concept",    limit: 255
+    t.string   "user_id",    limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
