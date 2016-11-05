b7afd906d1db338cb32a2656e983ae4d9c6d179a
naoymatsuda <naoy.matsuda@gmail.com>
2016-11-05 22:56:13 +0900

    fix prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,18 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototypes.create(prototype_params)
+    redirect_to root_path
+      # Prototype.create(title: prototype_params[:catch_copy], image: tweet_params[:image], text: tweet_params[:text])
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
 end
