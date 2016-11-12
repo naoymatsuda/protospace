0bf1fa5587a9875b1d55d0238dd745997ba234bf
naoymatsuda <naoy.matsuda@gmail.com>
2016-11-12 15:49:04 +0900

    fix prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @prototypes = Prototype.order(id: :desc)
   end
 
   def show
